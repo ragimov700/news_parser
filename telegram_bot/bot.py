94a964eba01c43cdb833b63cbedc173485ac486b
Sherif Ragimov <ragimov700@yandex.ru>
2024-01-08 14:56:55 +0400

    added .env file

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,22 +1,27 @@
+import io
+import os
 from typing import Optional, Dict
 
-from telegram import Bot
-import io
 from PIL import Image
+from dotenv import load_dotenv
+from telegram import Bot
+
+load_dotenv()
 
 
 class TelegramBot:
-    token = ...
-    chat_id = ...
+    token = os.getenv('TOKEN')
+    chat_id = os.getenv('CHAT_ID')
     bot = Bot(token)
 
     @classmethod
     def send_message(cls, message):
+        """Отправляем пост в телеграм"""
         cls.bot.send_message(chat_id=cls.chat_id, text=message)
 
     @classmethod
     def send_post(cls, image: Image, items: Optional[Dict[str, str]]) -> None:
-        """Обрабатываем данные и отправляем в телеграм."""
+        """Отправляем пост телеграм."""
         img_byte_arr = io.BytesIO()
         image.save(img_byte_arr, format='PNG')
         img_byte_arr.seek(0)
